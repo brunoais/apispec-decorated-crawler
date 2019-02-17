fb75f5ea94e5995541899be26ed4f06925a0861f
James Owen <jamesowen@outlook.com>
2019-02-17 21:19:46 +0100

    Tidy up README detail and plugin module header comment

--- README.md
+++ README.md
@@ -9,14 +9,14 @@ For basic and extended examples, please see the scripts in the examples subdirec
 ## Details
 
 This is an operations helper that allows you to pass a decorated view and get the combined documentation of all decorator functions.
-It required the view passed to `add_path`. Inspects view docstrings and its docd_wraps decorator functions and merges all the
-documentation into a single document.
-Useful if you use decorators to manage authentication or even if you have shared error pages and you do not
-want to document common error states (status 400, status 500, etc...) individually on all views.
+
+It requires the view function to be passed to `path`. The plugin inspects view docstrings and docstrings from `docd_wraps` decorator functions and merges all the documentation into a single document.
+
+This plugin is useful if you use decorators to manage authentication or even if you have shared error pages and you do not want to document common error states (status 400, status 500, etc...) individually on all views.
 
 All documentation is merged from bottom-up, starting on the view function and ending on the topmost decorator.
-Decorators can declare a "special" HTTP method called _ (underscore). That one is applied last for all methods,
-also from bottom up, in a subsequent pass.
+
+Decorators can declare a "special" HTTP method called `_` (underscore). This will be applied last for all HTTP methods, also from bottom up, in a subsequent pass.
 
 
 ## Installation
--- brunoais/apispec/ext/decorated_crawler.py
+++ brunoais/apispec/ext/decorated_crawler.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Operations helper that allows you to pass a decorated view and get the combined documentation of all decorator
-functions.
-It required the view passed to `add_path`. Inspects view docstrings and its docd_wraps decorator functions
-and merges all the documentation into a single document.
-Useful if you use decorators to manage authentication or even if you have shared error pages and you do not
-want to document common error states (status 400, status 500, etc...) individually on all views.
+This is an operations helper that allows you to pass a decorated view and get the combined documentation of all decorator functions.
+
+It requires the view function to be passed to `path`. The plugin inspects view docstrings and docstrings from `docd_wraps` decorator functions and merges all the documentation into a single document.
+
+This plugin is useful if you use decorators to manage authentication or even if you have shared error pages and you do not want to document common error states (status 400, status 500, etc...) individually on all views.
 
 All documentation is merged from bottom-up, starting on the view function and ending on the topmost decorator.
-Decorators can declare a special method called _ (underscore). That one is applied last for all methods,
-also from bottom up, in a subsequent pass.
+
+Decorators can declare a "special" HTTP method called `_` (underscore). This will be applied last for all HTTP methods, also from bottom up, in a subsequent pass.
 
 For example usage see the README.
 """
