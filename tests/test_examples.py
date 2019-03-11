5791a26bbabf962928294ef372d27a29fb727eb4
James Owen <james@cryptosense.com>
2019-03-11 16:13:37 +0100

    Move the tests to use fixtures

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,8 +7,8 @@ from openapi_spec_validator import validate_v2_spec_url
 
 # TODO: Auto-discover the example names
 EXAMPLES = [
-    ["basic_example"],
-    ["extended_example_with_marshmallow"],
+    "basic_example",
+    "extended_example_with_marshmallow",
 ]
 
 
@@ -17,21 +17,28 @@ def get_output_file_path(example_name):
     return os.path.abspath(file_path)
 
 
-def get_spec_and_expected_output(example_name):
+@pytest.fixture
+def output_file_path(request):
+    example_name = request.param
+    return get_output_file_path(example_name)
+
+
+@pytest.fixture
+def spec_and_expected_output(request):
+    example_name = request.param
     example_module = import_module("examples." + example_name)
     spec = example_module.spec
     output_file_path = get_output_file_path(example_name)
     return spec.to_yaml(), open(output_file_path).read()
 
 
-@pytest.mark.parametrize(['example_name'], EXAMPLES)
-def test_extended_example_with_marshmallow(example_name):
-    spec, expected = get_spec_and_expected_output(example_name)
+@pytest.mark.parametrize('spec_and_expected_output', EXAMPLES, indirect=True)
+def test_extended_example_with_marshmallow(spec_and_expected_output):
+    spec, expected = spec_and_expected_output
 
     assert yaml.load(spec) == yaml.load(expected)
 
 
-@pytest.mark.parametrize(['example_name'], EXAMPLES)
-def test_example_outputs_valid_openapiv2(example_name):
-    output_file_path = get_output_file_path(example_name)
+@pytest.mark.parametrize('output_file_path', EXAMPLES, indirect=True)
+def test_example_outputs_valid_openapiv2(output_file_path):
     validate_v2_spec_url("file://" + output_file_path)
